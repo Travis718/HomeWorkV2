8425db5c99be7e1d35cdf3d004e6cc1a1aa58a65
Travis <suporal@mail.ru>
2020-10-04 19:03:59 +0300

    p1

--- src/ru/geekbrains/lesson2/SecondLesson.java
+++ src/ru/geekbrains/lesson2/SecondLesson.java
@@ -1,5 +1,24 @@
 package ru.geekbrains.lesson2;
 
-public class SecondLesson {
+public class SecondLesson
+{
+    public static void main(String[] args) {
 
-}
+            int[] arr = new int[] {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+            System.out.print("Заданный массив: ");
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+                if (arr[i] == 0) arr[i] = 1;
+                else arr[i] = 0;
+            }
+            System.out.print("\nИзмененный массив:  ");
+            for (int i : arr) {
+                System.out.print(i + " ");
+            }
+
+
+        }
+
+    }
+
+
