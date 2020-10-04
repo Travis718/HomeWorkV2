d0611d4964d98cb233d24c860b23e02f1d7ceba8
Travis <suporal@mail.ru>
2020-10-04 21:19:42 +0300

    p.2 исправленный

--- src/ru/geekbrains/lesson2/SecondLesson.java
+++ src/ru/geekbrains/lesson2/SecondLesson.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 public class SecondLesson
 {
     public static void main(String[] args) {
-    // п.1 создаем массив и меняем значения его членов
+
+        // п.1 создаем массив и меняем значения его членов
             int[] arr = new int[] {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
             System.out.print("Заданный массив: ");
             for (int i = 0; i < arr.length; i++) {
@@ -29,24 +30,31 @@ public class SecondLesson
         /* выдал интересную ошибку. причину так и не нашел:
         Заданный массив: 1 5 3 2 11 4 5 2 4 8 9 1
         Измененный массив:  2 10 6 4 11 8 10 4 8 8 9 2
-*/
+        */
         for (int i = 0; i < ar.length; i++) {
             System.out.print(ar[i] + " ");
         }
 
+        System.out.println("\n Task 2");
+        int[] array = new int[8];
+        changeArray(array);
+        System.out.print(array + " ");
 
     }
+
+
     // п.2 создаем массив заданного размера и циклом заполняем его
 
-    // В рамках метода Main выдает ошибку. за пределами метода - результат не выводится в консоль!
-    public static void changeArray(int[] inputArray) {
-        int[] array = new int[8];
+        static void changeArray(int[] inputArray) {
         System.out.print("\n Новый массив: ");
-        for (int i = 0, j = 0; i < inputArray.length; i++, j += 3) {
+        for (int i = 0; i < inputArray.length; i++) {
+            inputArray[i] = i *3;
             System.out.print(inputArray[i] + " ");
+            // результат в консоли:  Новый массив: 0 3 6 9 12 15 18 21 [I@5b6f7412
         }
     }
-    // п.3 задаем массив и проходимся по нему циклом
+
+    // п.4 Задаем квадратную матрицу и заполняем ее диагонали единицами
 
 
 }
